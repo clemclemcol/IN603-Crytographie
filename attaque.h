971f532eef366f44f49b593545907b70863800dd
clemclemcol <83111908+clemclemcol@users.noreply.github.com>
2021-05-07 17:06:32 +0200

    Add files via upload

--- attaque.h
+++ attaque.h
@@ -3,19 +3,21 @@
  typedef struct keylist
 {
     char *key;
-    char *res;
+    char *resCh;
+    char *resDch;
+
     struct keylist *suiv;
 
 } KEYLIST;
 
-void create_keytab(int size, KEYLIST *tab);
 
-void combine(char *nb, char *tmp, int len, int ind);
 
-void keys_generator(int size, KEYLIST *tab);
+char *dec_to_hex(int num, char *numhex);
 
-void copy_key_tab(int size, KEYLIST *rempli, KEYLIST *vide);
+KEYLIST* create_tabs(KEYLIST *key_tab);
+
+KEYLIST* key_generator(int size, KEYLIST *key_tab);
 
 void comparison_tabs(KEYLIST *tab);
 
-void attaque(int size, KEYLIST *tab1, KEYLIST *tab2, char *mess, char * chif);
+void attaque(int size, KEYLIST *key_tab, char *m1, char *c1);
