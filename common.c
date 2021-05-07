2abee5a68bdded92bd3097038e2e08f8e333fd86
clemclemcol <83111908+clemclemcol@users.noreply.github.com>
2021-05-07 17:07:02 +0200

    Add files via upload

--- common.c
+++ common.c
@@ -30,17 +30,17 @@ int dec_to_bi(int num, int *num_bi)
         //printf("tmp %d\n", tmp[i]);
         num = num / 2;
     }
-    
-    for (j = 0; j < 5; j ++)
+
+    for (j = 0; j < 5; j++)
     {
-        if (tmp[j]){
-           num_bi[j] = tmp[j]; 
-          
+        if (tmp[j])
+        {
+            num_bi[j] = tmp[j];
         }
-        else {
+        else
+        {
             num_bi[j] = 0;
         }
-        
     }
 
     free(tmp);
@@ -49,68 +49,87 @@ int dec_to_bi(int num, int *num_bi)
 
 int hex_to_bi(char *key, char *key_bi)
 {
-    if (strcmp(key, "10") == 0)
+    char dix[3] = "10";
+    char ze[2] = "\0";
+    if (key == dix)
     {
         strcat(key_bi, "1010");
         return 0;
     }
-    for (int i = 0; key[i] != '\0'; i++)
+    int i = 0;
+    while (key[i])
     {
         switch (key[i])
         {
         case '0':
             strcat(key_bi, "0000");
+            i++;
             break;
         case '1':
             strcat(key_bi, "0001");
+            i++;
             break;
         case '2':
             strcat(key_bi, "0010");
+            i++;
             break;
         case '3':
             strcat(key_bi, "0011");
+            i++;
             break;
         case '4':
             strcat(key_bi, "0100");
+            i++;
             break;
         case '5':
             strcat(key_bi, "0101");
+            i++;
             break;
         case '6':
             strcat(key_bi, "0110");
+            i++;
             break;
         case '7':
             strcat(key_bi, "0111");
+            i++;
             break;
         case '8':
             strcat(key_bi, "1000");
+            i++;
             break;
         case '9':
             strcat(key_bi, "1001");
+            i++;
             break;
         case 'a':
         case 'A':
             strcat(key_bi, "1010");
+            i++;
             break;
         case 'b':
         case 'B':
             strcat(key_bi, "1011");
+            i++;
             break;
         case 'c':
         case 'C':
             strcat(key_bi, "1100");
+            i++;
             break;
         case 'd':
         case 'D':
             strcat(key_bi, "1101");
+            i++;
             break;
         case 'e':
         case 'E':
             strcat(key_bi, "1110");
+            i++;
             break;
         case 'f':
         case 'F':
             strcat(key_bi, "1111");
+            i++;
             break;
         default:
             fprintf(stderr, "Invalid hexadecimal input.\n");
@@ -125,79 +144,81 @@ int bi_to_hex(char *bi, char *hex)
     for (int i = 0; i < strlen(bi); i = i + 4)
     {
         char *tmp = malloc(4 * sizeof(char));
-        if (tmp == NULL){
+        if (tmp == NULL)
+        {
             fprintf(stderr, "ERROR malloc.\n");
         }
-        if (strlen(tmp)!= 0){
+        if (strlen(tmp) != 0)
+        {
             strcpy(tmp, "");
         }
-      
+
         int j;
         for (j = i; j < i + 4; j++)
         {
             strncat(tmp, &bi[j], 1);
         }
-        
+
         for (int w = 0; w < 16; w++)
         {
             char *wS = malloc(sizeof(char));
             int value = strtoul(tmp, NULL, 2);
             switch (value)
-                {
-                case 0:
-                    strcat(hex, "0");
-                    break;
-                case 1:
-                    strcat(hex, "1");
-                    break;
-                case 2:
-                    strcat(hex, "2");
-                    break;
-                case 3:
-                    strcat(hex, "3");
-                    break;
-                case 4:
-                    strcat(hex, "4");
-                    break;
-                case 5:
-                    strcat(hex, "5");
-                    break;
-                case 6:
-                    strcat(hex, "6");
-                    break;
-                case 7:
-                    strcat(hex, "7");
-                    break;
-                case 8:
-                    strcat(hex, "8");
-                    break;
-                case 9:
-                    strcat(hex, "9");
-                    break;
-                case 10:
-                    strcat(hex, "a");
-                    break;
-                case 11:
-                    strcat(hex, "b");
-                    break;
-                case 12:
-                    strcat(hex, "c");
-                    break;
-                case 13:
-                    strcat(hex, "d");
-                    break;
-                case 14:
-                    strcat(hex, "e");
-                    break;
-                case 15:
-                    strcat(hex, "f");
-                    break;
-                default:
-                    fprintf(stderr, "Unknown value.\n");
-                    break;
-                }
+            {
+            case 0:
+                strcat(hex, "0");
                 break;
-            
+            case 1:
+                strcat(hex, "1");
+                break;
+            case 2:
+                strcat(hex, "2");
+                break;
+            case 3:
+                strcat(hex, "3");
+                break;
+            case 4:
+                strcat(hex, "4");
+                break;
+            case 5:
+                strcat(hex, "5");
+                break;
+            case 6:
+                strcat(hex, "6");
+                break;
+            case 7:
+                strcat(hex, "7");
+                break;
+            case 8:
+                strcat(hex, "8");
+                break;
+            case 9:
+                strcat(hex, "9");
+                break;
+            case 10:
+                strcat(hex, "a");
+                break;
+            case 11:
+                strcat(hex, "b");
+                break;
+            case 12:
+                strcat(hex, "c");
+                break;
+            case 13:
+                strcat(hex, "d");
+                break;
+            case 14:
+                strcat(hex, "e");
+                break;
+            case 15:
+                strcat(hex, "f");
+                break;
+            default:
+                fprintf(stderr, "Unknown value bi_to_hex.\n");
+                break;
+            }
+            break;
+
             free(wS);
         }
         free(tmp);
@@ -209,27 +230,31 @@ int bi_to_hex(char *bi, char *hex)
 char *pivot(char *key_bi)
 {
     char *tab_piv = malloc(80 * sizeof(char));
-    for (int i = 79; i >= 0; --i){
-        if ((i - 61) >= 0){
-            strncpy(&tab_piv[i - 61], &key_bi[i],1);
+    for (int i = 79; i >= 0; --i)
+    {
+        if ((i - 61) >= 0)
+        {
+            strncpy(&tab_piv[i - 61], &key_bi[i], 1);
         }
-        else if ((i - 61) < 0){
+        else if ((i - 61) < 0)
+        {
             int j = (i - 61) + 80;
-            strncpy(&tab_piv[j], &key_bi[i],1);
+            strncpy(&tab_piv[j], &key_bi[i], 1);
         }
     }
     empty(key_bi);
-    for(int i = 0; i < 80; i++){
-        strncpy(&key_bi[i], &tab_piv[i],1);
+    for (int i = 0; i < 80; i++)
+    {
+        strncpy(&key_bi[i], &tab_piv[i], 1);
     }
     free(tab_piv);
     return key_bi;
 }
 
-char* key_xor(char *key_bi, int I)
-{ 
+char *key_xor(char *key_bi, int I)
+{
     //fprintf(stdout, "key : %s --> tour : %d\n", key_bi, I);
-    char* to_xor = malloc(sizeof(char)*5);
+    char *to_xor = malloc(sizeof(char) * 5);
     int w = 0;
     for (int x = 60; x < 65; x++)
     {
@@ -257,10 +282,21 @@ char* key_xor(char *key_bi, int I)
     return key_bi;
 }
 
-char * mess_xor(char *state, char *ki){
-    if (strlen(ki) != strlen(state)){
+char *mess_xor(char *state, char *ki)
+{
+    printf("mess : %s --> %ld // key : %s --> %ld\n", state, strlen(state), ki, strlen(ki));
+
+    if (strlen(ki) > 24)
+    {
+        strcpy(&ki[23], "\0");
+        printf("la\n");
+        //strlen(ki) = strlen(ki)-1;
+    }
+
+    if (strlen(ki) != strlen(state) )//&&strlen(ki) > strlen(state)+1
+    {
         fprintf(stderr, "Error lenght arguments message and ki.\n");
-        exit (1);
+        exit(1);
     }
 
     int *stateI = malloc(24 * sizeof(int));
@@ -273,18 +309,16 @@ char * mess_xor(char *state, char *ki){
         kiI[i] = atoi(&kTemp[i]);
         sprintf(&sTemp[i], "%c", state[i]);
         stateI[i] = atoi(&sTemp[i]);
-
     }
-    
+
     for (int i = 0; i < strlen(state); i++)
     {
         stateI[i] = stateI[i] ^ kiI[i];
-        
     }
-    
+
     for (int i = 0; i < strlen(state); i++)
     {
-        char * tmp = malloc(sizeof(char));
+        char *tmp = malloc(sizeof(char));
         sprintf(tmp, "%d", stateI[i]);
         strncpy(&state[i], tmp, 1);
         free(tmp);
@@ -292,5 +326,4 @@ char * mess_xor(char *state, char *ki){
     free(stateI);
     free(kiI);
     return state;
-
 }
